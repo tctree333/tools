a540f6ac74a60e216cd07bdf7f4b4140efd2f3e5
Tomi Chen <tctree333@users.noreply.github.com>
2022-04-02 13:10:11 -0700

    bootstrap

---
+++ svelte.config.js
@@ -0,0 +1,15 @@
+import adapter from '@sveltejs/adapter-netlify';
+import preprocess from 'svelte-preprocess';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: preprocess(),
+
+	kit: {
+		adapter: adapter({
+			split: false
+		})
+	}
+};
+
+export default config;

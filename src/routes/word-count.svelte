b1fe5e0a0be10baffadae255719981e974928d55
Tomi Chen <tctree333@users.noreply.github.com>
2022-04-02 14:34:21 -0700

    word count

---
+++ src/routes/word-count.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	let value = '';
+	let boundaries = '\\s—–/,';
+
+	let boundaryRegex: RegExp;
+	$: {
+		try {
+			boundaryRegex = new RegExp(`[${boundaries}]+`);
+		} catch (e) {}
+	}
+	$: words = value ? value.split(boundaryRegex).length : 0;
+	$: characters = value ? value.length : 0;
+</script>
+
+<textarea bind:value />
+<label>
+	Word boundaries:
+	<input type="text" bind:value={boundaries} />
+</label>
+<p>Words: {words}</p>
+<p>Characters: {characters}</p>

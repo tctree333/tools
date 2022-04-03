5a62a2c7232ed5f67a35c690a2f38839b278eb5f
Tomi Chen <tctree333@users.noreply.github.com>
2022-04-03 11:25:03 -0700

    add template

---
+++ src/routes/_template.svelte
@@ -0,0 +1,14 @@
+<script context="module" lang="ts">
+	export const metadata = {
+		title: 'Title Here',
+		description: 'Description placeholder'
+	};
+</script>
+
+<script lang="ts">
+	import Head from '$lib/components/Head.svelte';
+</script>
+
+<Head title={metadata.title} description={metadata.description} />
+
+<h1>Wow!</h1>

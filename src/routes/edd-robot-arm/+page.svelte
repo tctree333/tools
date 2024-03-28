aa5a2b866bcf09422e68c9a2891403fc82ac626c
Tomi Chen <tctree333@users.noreply.github.com>
2024-03-28 10:10:23 -0700

    add keyboard control

--- src/routes/edd-robot-arm/+page.svelte
+++ src/routes/edd-robot-arm/+page.svelte
@@ -33,13 +33,14 @@
 		reset();
 	};
 
-	let lastMessage: string = '';
+	let lastMessages: string[] = ['', '', '', '', ''];
 	async function readLoop() {
 		// Reads data from the input stream and displays it in the console.
 		while (true) {
 			const { value, done } = await reader.read();
 			console.log('DEBUG', 'serial in', [value]);
-			lastMessage = value;
+			lastMessages.unshift(value);
+			lastMessages.pop();
 
 			if (done) {
 				console.log('[readLoop] DONE', done);
@@ -58,23 +59,59 @@
 	}
 
 	const send = async (data: string): Promise<string> => {
-		console.log(data);
+		const sentry = Math.random().toFixed(5).slice(2);
+		const packet = `${data} ${sentry}\r\n`;
+		console.log('DEBUG', 'serial out', [packet]);
 		const writer = writerStream.getWriter();
-		await writer.write(data + '\x0D');
+		await writer.write(packet);
 		writer.releaseLock();
 
-		return lastMessage;
+		const returned = await new Promise<string>((resolve) =>
+			setTimeout(() => {
+				resolve(lastMessages.find((msg) => msg.trim().endsWith(sentry))?.split(' ')[0] ?? '');
+			}, 100)
+		);
+
+		return returned;
 	};
 
 	let positions = new Map<string, number>();
 	const move = async (motor: string, step: number) => {
-		const newPos = await send(`${motor}${step > 0 ? '+' : '-'}${Math.abs(step)}\n`);
+		const newPos = await send(`${motor}${step > 0 ? '+' : '-'}${Math.abs(step)}`);
+
+		if (isNaN(parseFloat(newPos))) return;
+
 		positions.set(motor, parseFloat(newPos));
 		positions = positions;
+	};
+
+	const handleKeypress = async (event: KeyboardEvent) => {
+		if (!port) return;
+
+		const key = event.key;
+		if (key === 'q') {
+			move('s', 5);
+		} else if (key === 'a') {
+			move('s', -5);
+		} else if (key === 'w') {
+			move('0', 5);
+		} else if (key === 's') {
+			move('0', -5);
+		} else if (key === 'e') {
+			move('1', 5);
+		} else if (key === 'd') {
+			move('1', -5);
+		} else if (key === 'r') {
+			move('2', 5);
+		} else if (key === 'f') {
+			move('2', -5);
+		}
 	};
 </script>
 
 <Head title={metadata.title} description={metadata.description} />
+
+<svelte:window on:keypress={handleKeypress} />
 
 <h1>Robot Arm</h1>
 
@@ -86,17 +123,18 @@
 	>
 	<div class="flex flex-row space-x-6">
 		{#each ['s', '0', '1', '2'] as motor}
-			<div class="flex flex-col space-y-4">
+			<div class="flex flex-col space-y-4 items-center">
+				<span>Motor {motor}:</span>
 				<button
 					on:click={() => move(motor, 5)}
 					type="button"
-					class="px-4 py-0.5 border-2 border-stone-400">+</button
+					class="m-0 px-4 py-0.5 border-2 border-stone-400">+</button
 				>
-				{positions.get(motor) ?? 0}
+				<span>{positions.get(motor) ?? 0}</span>
 				<button
 					on:click={() => move(motor, -5)}
 					type="button"
-					class="px-4 py-0.5 border-2 border-stone-400">-</button
+					class="m-0 px-4 py-0.5 border-2 border-stone-400">-</button
 				>
 			</div>
 		{/each}
